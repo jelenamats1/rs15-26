90986faf4f7d8e0cc0cda2642861cf3226d4adb5
jelenamats1 <jelenapajic7@gmail.com>
2015-06-02 13:39:52 +0200

    nove ideje

---
+++ T-Rex/Gui/diagram.cpp
@@ -0,0 +1,6 @@
+#include "diagram.h"
+
+Diagram::Diagram(QObject *parent) :
+    QFrame(parent)
+{
+}
---
+++ T-Rex/Gui/diagram.h
@@ -0,0 +1,18 @@
+#ifndef DIAGRAM_H
+#define DIAGRAM_H
+
+#include <QFrame>
+
+class Diagram : public QFrame
+{
+    Q_OBJECT
+public:
+    explicit Diagram(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // DIAGRAM_H
